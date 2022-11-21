3488f1414214f75332f29a4949f9b6cea2873e56
sosuisen <hidekazu.kubota@gmail.com>
2022-11-21 17:39:03 +0900

    updated

--- test10.js
+++ test10.js
@@ -2,4 +2,4 @@ const array = [{ id: 1 } , { id: 2 }];
 const obj = array.find(elm => {
   if (elm.id === 2) return true;
 })
-console.log(obj)
+console.log(obj); // { id: 2 }
--- test9.js
+++ test9.js
@@ -8,6 +8,6 @@ const resource2 = '/todos/hello'; // 文字なので \dにマッチしない
 const found2 = resource2.match(re);
 console.log(found2); // null 
 
-const resource3 = '/todos/3/id'; // 数字で終わってないため $とマッチしない
+const resource3 = '/todos/3/title'; // 数字で終わってないため $とマッチしない
 const found3 = resource3.match(re);
 console.log(found3); // null        
